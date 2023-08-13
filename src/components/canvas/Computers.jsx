406cbec72d9933fe8f72e68b0a24211634c39b55
mangalgithub <127006680+mangalgithub@users.noreply.github.com>
2023-08-13 13:47:31 +0530

    Bug fixed

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -36,7 +36,7 @@ const ComputersCanvas = () => {
 
   useEffect(() => {
     // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia('(max-width:450px)');
+    const mediaQuery = window.matchMedia('(max-width:310px)');
 
     // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
